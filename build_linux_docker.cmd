956ca4015e8dd99753370fda5f130efdd1a7f012
Andrey Koloskov <andreykoloskov.ru@gmail.com>
2020-05-31 18:56:31 +0300

    01_helloworld

--- build_linux_docker.cmd
+++ build_linux_docker.cmd
@@ -1,6 +1,46 @@
-docker build -t helloworld:v1 .
+if exist packages (
+    rd /s /q packages
+)
+md packages
 
-docker run helloworld:v1
+docker build -f ./DockerfileUbuntu -t andreykoloskov/helloworld:v1 .
+docker run --rm --name helloworld -d andreykoloskov/helloworld:v1 /bin/sh -c "while true; do sleep 1; done"
+docker cp helloworld:/app/build_linux/deb/ packages/
+docker stop helloworld
+docker run --rm --name helloworld andreykoloskov/helloworld:v1
 
-rem docker rm $(docker ps -a -q)
-rem docker rmi $(docker images -a -q)
+docker build -f ./DockerfileUbuntuTest -t andreykoloskov/helloworld:v2 .
+docker run --rm --name helloworld andreykoloskov/helloworld:v2
+
+
+:: docker run -it helloworld /bin/bash
+:: docker rm $(docker ps -a -q)
+:: docker rmi -f $(docker images -a -q)
+:: docker rmi -f 1233
+
+:: docker images -a
+
+:: https://dker.ru/docs/
+
+:: docker run ubuntu /bin/echo 'Hello world'
+:: docker run -t -i ubuntu /bin/bash
+:: docker run -d ubuntu /bin/sh -c "while true; do echo hello world; sleep 1; done"
+:: docker ps
+:: docker logs insane_babbage
+:: docker stop insane_babbage
+
+:: docker run -d -P --name web training/webapp python app.py
+:: docker run -d -p 80:5000 training/webapp python app.py
+:: docker ps -l
+:: docker inspect web
+:: docker port nostalgic_morse 5000
+:: docker logs -f nostalgic_morse
+:: docker top nostalgic_morse
+:: docker inspect nostalgic_morse
+
+:: docker search centos
+:: docker commit -m "Added json gem" -a "Kate Smith" 0b2616b0e5a8 ouruser/sinatra:v2
+:: docker push andreykoloskov/helloworld
+:: docker pull andreykoloskov/helloworld
+
+:: docker network ls
